f06147dd8ea8ee88bf73d47085f9538b077c1d68
wintus <yuya0726+GitHub@gmail.com>
2019-10-31 18:02:28 +0900

    Use firebase auth (reset on redirect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext, useCallback } from "react";
 import "./App.css";
+import { FirebaseContext } from "./firebase";
+import { FirebaseAuth, signInWithRedirect, signOut } from "./FirebaseAuth";
+
+const Content: React.FC = () => {
+  const { userId, userName } = useContext(FirebaseContext);
+  return (
+    <div>
+      <span>
+        {userName} ({userId}) is signed in
+      </span>
+    </div>
+  );
+};
 
 const App: React.FC = () => {
-  return <div></div>;
+  const NotSignedIn = useCallback(() => {
+    return <button onClick={() => signInWithRedirect()}>sign in</button>;
+  }, []);
+  const Loading = useCallback(() => {
+    return (
+      <div>
+        <span>loading now……</span>
+      </div>
+    );
+  }, []);
+
+  return (
+    <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
+      <Content />
+      <div>
+        <button onClick={signOut}>sign out</button>
+      </div>
+    </FirebaseAuth>
+  );
 };
 
 export default App;
