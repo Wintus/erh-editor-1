1e81ae84adab652e0bf7de70580cd5bff725a96b
wintus <yuya0726+GitHub@gmail.com>
2019-11-05 20:11:52 +0900

    Add editor in page

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,3 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 
-export default ({ textId }: { textId: string }) => <div>pages/Edit</div>;
+import { Link } from "../Router";
+import { Document, useDatabaseDocument } from "../firebase-database";
+
+const Input = styled.textarea`
+  width: 100%;
+  height: 90vh;
+  ::focus {
+    outline: none;
+  }
+  font-size: 16px;
+  font-family: "monospace";
+`;
+
+export default ({ textId }: Pick<Document, "textId">) => {
+  const { text, updateText, loaded, pending } = useDatabaseDocument(textId);
+
+  if (!loaded) {
+    return <div>now loading……</div>;
+  } else {
+    return (
+      <div>
+        <div>{pending ? "write is pending now" : ""}</div>
+        <Link as="button" href="/">
+          Back to list
+        </Link>
+        <Input
+          value={text}
+          onChange={e => updateText(e.target.value)}
+          autoFocus
+        />
+      </div>
+    );
+  }
+};
