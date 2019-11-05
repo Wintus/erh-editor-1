5dc17ad18b0c1a73154ec6d21a678e6b92bcb8b6
wintus <yuya0726+GitHub@gmail.com>
2019-11-05 14:06:18 +0900

    Add memoized router

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,9 @@ import React, {
 import { History } from "history";
 import uuidv4 from "uuidv4";
 
+import Index from "./pages/Index";
+import Edit from "./pages/Edit";
+
 const useCurrentPath = (history: History): string => {
   const [pathname, setPathname] = useState(history.location.pathname);
 
@@ -17,4 +20,21 @@ const useCurrentPath = (history: History): string => {
   }, [history]);
 
   return pathname;
+};
+
+const useEditorRouting = (pathname: string): JSX.Element => {
+  const content = useMemo(() => {
+    if (pathname === "/") {
+      return <Index />;
+    } else {
+      const id = pathname.slice(1);
+      if (uuidv4.is(id)) {
+        return <Edit textId={id} />;
+      } else {
+        return <div>404 not found</div>;
+      }
+    }
+  }, [pathname]);
+
+  return content;
 };
---
+++ src/pages/Edit.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default ({ textId }: { textId: string }) => <div>pages/Edit</div>;
